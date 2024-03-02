b30cd7b848d1876bf9cb6c6b2fdafa419303bc87
jmquilario <jmquilario@gmail.com>
2024-03-02 11:39:41 +0000

    fix: tamagui config

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,23 +1,137 @@
-// the v2 config imports the css driver on web and react-native on native
+// @tamagui/core doesn't include `createMedia` so that it can avoid
 
-// for reanimated: @tamagui/config/v2-reanimated
+// a dependency on react-native. If you are using tamagui, you can
+// import createMedia from there directly and avoid this line:
+// import {createMedia} from '@tamagui/react-native-media-driver';
 
-// for react-native only: @tamagui/config/v2-native
+import {createFont, createMedia, createTamagui, createTokens} from 'tamagui';
+// Create a font:
+// To work with the tamagui UI kit styled components (which is optional)
+// you'd want the keys used for `size`, `lineHeight`, `weight` and
+// `letterSpacing` to be consistent. The `createFont` function
+// will fill-in any missing values if `lineHeight`, `weight` or
+// `letterSpacing` are subsets of `size`.
+const interFont = createFont({
+  family: 'Inter, Helvetica, Arial, sans-serif',
+  size: {
+    1: 12,
+    2: 14,
+    3: 15,
+  },
+  lineHeight: {
+    // 1 will be 22
+    2: 22,
+  },
+  weight: {
+    1: '300',
+    // 2 will be 300
+    3: '600',
+  },
+  letterSpacing: {
+    1: 0,
+    2: -1,
+    // 3 will be -1
+  },
 
-import {config} from '@tamagui/config/v2-native';
+  // (native only) swaps out fonts by face/style
 
-import {Text, View} from 'react-native';
+  face: {
+    300: {normal: 'InterLight', italic: 'InterItalic'},
+    600: {normal: 'InterBold'},
+  },
+});
 
-import {createTamagui} from '@tamagui/core'; // or '@tamagui/core'
-const appConfig = createTamagui(config);
-export type AppConfig = typeof appConfig;
-declare module '@tamagui/core' {
-  // or '@tamagui/core'
+// Set up our tokens
+// The keys can be whatever you want, but we do recommend keeping them
+// consistent across the different token categories and intended for
+// usage together to make nice designs - eg for a Button to use.
+const size = {
+  0: 0,
+  1: 5,
+  2: 10,
+  small: 20,
+  medium: 30,
+  true: 30, // note true = 30 just like medium, your default size token
+  large: 40,
+  // ....
+};
 
-  // overrides TamaguiCustomConfig so your custom types
+export const tokens = createTokens({
+  size,
+  space: {...size, '-1': -5, '-2': -10},
+  radius: {0: 0, 1: 3},
+  zIndex: {0: 0, 1: 100, 2: 200},
+  color: {
+    white: '#fff',
+    black: '#000',
+    green: '#00FF00',
+  },
+});
 
-  // work everywhere you import `tamagui`
+const config: ReturnType<typeof createTamagui> = createTamagui({
+  fonts: {
+    // for tamagui, heading and body are assumed
+    heading: interFont,
+    body: interFont,
+  },
+  tokens,
+  // For more on themes, see the Themes page
+  themes: {
+    light: {
+      bg: '#f2f2f2',
+      color: tokens.color.black,
+    },
+    dark: {
+      bg: '#111',
+      color: tokens.color.white,
+    },
+  },
+  // For web-only, media queries work out of the box and you can avoid the
+  // `createMedia` call here by passing the media object directly.
+  // If you are going to target React Native, use `createMedia` (it's an identity
+  // function on web so you can import it there without concern).
 
+  media: createMedia({
+    sm: {maxWidth: 860},
+    gtSm: {minWidth: 860 + 1},
+    short: {maxHeight: 820},
+    hoverNone: {hover: 'none'},
+    pointerCoarse: {pointer: 'coarse'},
+  }),
+
+  // Shorthands
+  // Adds <View m={10} /> to <View margin={10} />
+  // See Settings section on this page to only allow shorthands
+  // Be sure to have `as const` at the end
+
+  shorthands: {
+    px: 'paddingHorizontal',
+    f: 'flex',
+    m: 'margin',
+    w: 'width',
+  } as const,
+
+  // Change the default props for any styled() component with a name.
+  // We are discouraging the use of this and have deprecated it, prefer to use
+  // styled() on any component to change it's styles.
+
+  defaultProps: {
+    Text: {
+      color: tokens.color.green,
+    },
+  },
+});
+
+type AppConfig = typeof config;
+// this will give you types for your components
+// note - if using your own design system, put the package name here instead of tamagui
+
+declare module 'tamagui' {
   interface TamaguiCustomConfig extends AppConfig {}
+  // if you want types for group styling props, define them like so:
+
+  interface TypeOverride {
+    groupNames(): 'a' | 'b' | 'c';
+  }
 }
-export default appConfig;
+export default config;
